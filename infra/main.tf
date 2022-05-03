87943658d75c701750401cd516cf41b99a054014
Chandan Gulati <33059635+Chandang786@users.noreply.github.com>
2022-05-03 23:28:03 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -5,11 +5,7 @@ module "securitygroups" {
   description = var.sg_description
 }
 
-variable "accesskey"{
-}
-variable "secret_key"{
-}  
-  
+
 module "mysqlrds" {
   source                 = "../module/rds"
   db_name                = var.db_name
